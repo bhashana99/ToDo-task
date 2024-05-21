fe3694293fdf79a7685f5ff80864bf82af382962
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2024-05-21 17:56:24 +0530

    create fetch todos API Route

--- back-end/controllers/todo.controller.js
+++ back-end/controllers/todo.controller.js
@@ -8,3 +8,12 @@ export const createNewTodo = async (req, res) => {
         console.log(error);
     }
 }
+
+export const getTodos = async (req, res) => {
+    try {
+        const todos = await Todo.find();
+        return res.status(200).json(todos);
+    } catch (error) {
+        console.log(error);
+    }
+}
--- back-end/routes/todo.route.js
+++ back-end/routes/todo.route.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { createNewTodo } from '../controllers/todo.controller.js';
+import { createNewTodo,getTodos } from '../controllers/todo.controller.js';
 
 const router = express.Router();
 
 router.post('/create-todo', createNewTodo);
+router.get('/get-todos', getTodos);
 
 export default router;
